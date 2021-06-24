958e325d184a693dd7c0270bc2229e20f361b63d
littleblackdot <2602690396@qq.com>
2021-06-24 23:17:41 +0800

    4650gVirtualMachine

--- 6.19/ParkingSystem/dataParse.h
+++
@@ -1,7 +0,0 @@
-#ifndef _TIME_H__
-#define _TIME_H__
-
-void getDate(const char *date, int *pmouth, int *date, int *year);
-void getTime(const char *time, int *phour, int *pminute);
-//int isLegalCarNumber(const char* carNumber);
-#endif
